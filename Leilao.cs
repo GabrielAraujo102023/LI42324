fcff43824e330f5ad0ff0fd76748488fd26a46e8
rickardo18 <a70492@uminho.pt>
2024-01-13 15:57:43 +0000

    leilao start

---
+++ Leilao.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace SpinToWin
+{
+    internal class Leilao
+    {
+        public string Estado { get; set; }
+        public int? Comprador { get; set; }
+        public float? ValorBase { get; set; }
+        public float? ValorMinimo { get; set; }
+        public float? PrecoVenda { get; set; }
+        public int Vendedor { get; set; }
+
+        // Default constructor
+        public Leilao()
+        {
+        }
+
+        // Constructor with parameters
+        public Leilao(string estado, int? comprador, float? valorBase, float? valorMinimo, float? precoVenda, int vendedor)
+        {
+            Estado = estado;
+            Comprador = comprador;
+            ValorBase = valorBase;
+            ValorMinimo = valorMinimo;
+            PrecoVenda = precoVenda;
+            Vendedor = vendedor;
+        }
+
+        // Copy constructor
+        public Leilao(Leilao other)
+        {
+            Estado = other.Estado;
+            Comprador = other.Comprador;
+            ValorBase = other.ValorBase;
+            ValorMinimo = other.ValorMinimo;
+            PrecoVenda = other.PrecoVenda;
+            Vendedor = other.Vendedor;
+        }
+
+        public override string ToString()
+        {
+            return $"Estado: {Estado}, Comprador: {Comprador}, " +
+                   $"ValorBase: {ValorBase}, ValorMinimo: {ValorMinimo}, PrecoVenda: {PrecoVenda}, Vendedor: {Vendedor}";
+        }
+    }
+}
---
+++ LeilaoDAO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.Data.SqlClient;
+using Microsoft.Identity.Client;
+
+
+namespace SpinToWin
+{
+    internal class LeilaoDAO
+    {
+        private static string connectionString = "Data Source=DESKTOP-AQDL94R;Initial Catalog=VinilDB;Integrated Security=True;TrustServerCertificate=True;";
+
+        public List<Leilao> GetListLeiloes()
+        {
+            List<Leilao> leiloes = new List<Leilao>();
+            Leilao leilao = null;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string query = "SELECT * FROM Leilao";
+                    using (SqlCommand cmd = new SqlCommand(query, connection))
+                    using (SqlDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            // Access data using reader
+                            string estado = reader.GetString(reader.GetOrdinal("Estado"));
+                            int? comprador = reader.IsDBNull(reader.GetOrdinal("Comprador")) ? (int?)null : reader.GetInt32(reader.GetOrdinal("Comprador"));
+                            float? valorBase = reader.IsDBNull(reader.GetOrdinal("Valor_base")) ? (float?)null : reader.GetFloat(reader.GetOrdinal("Valor_base"));
+                            float? valorMinimo = reader.IsDBNull(reader.GetOrdinal("Valor_minimo")) ? (float?)null : reader.GetFloat(reader.GetOrdinal("Valor_minimo"));
+                            float? precoVenda = reader.IsDBNull(reader.GetOrdinal("Preco_venda")) ? (float?)null : reader.GetFloat(reader.GetOrdinal("Preco_venda"));
+                            int vendedor = reader.GetInt32(reader.GetOrdinal("Vendedor"));
+
+                            leilao = new Leilao(estado, comprador, valorBase, valorMinimo, precoVenda, vendedor);
+                            MessageBox.Show(leilao.ToString());
+                            leiloes.Add(leilao);
+                        }
+                    }
+                    connection.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error: {ex.Message}");
+                // Handle the exception as needed
+            }
+            return leiloes;
+        }
+
+    }
+}
