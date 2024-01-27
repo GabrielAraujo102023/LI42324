381c2abf21057daa382b82d23c32d7487981af57
Filipe Oliveira <filipefelix10@hotmail.com>
2024-01-27 20:54:40 +0000

    Comprar_Leilao_Form

---
+++ Comprar_Leilao_Form.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpinToWin
+{
+    public partial class Comprar_Leilao_Form : Form
+    {
+        public Comprar_Leilao_Form()
+        {
+            InitializeComponent();
+        }
+    }
+}
