57062476d6734ad23be727c1a3ea7a2dabdc2e41
rickardo18 <a70492@uminho.pt>
2024-01-28 12:44:28 +0000

    add vinis DB

--- obj/Debug/net6.0-windows/SpinToWin.AssemblyInfo.cs
+++ obj/Debug/net6.0-windows/SpinToWin.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SpinToWin")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37c199d97eb110273fda581247f33df72294beeb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+204dcd50a1bb04f37d5faefde9f65ec06605d58d")]
 [assembly: System.Reflection.AssemblyProductAttribute("SpinToWin")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SpinToWin")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
