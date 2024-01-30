a7d85aa3ec458cc6c0db8af23d5734057557be10
João Ferreira <joaonqferreira@gmail.com>
2024-01-30 16:28:54 +0000

    Login bug and path to email credentials solved

--- Login_Form.cs
+++ Login_Form.cs
@@ -26,10 +26,13 @@ namespace SpinToWin
         {
             if (areTextBoxesEmpty()) return;
             Client client = clientDAO.GetClientByEmail(textBox1.Text);
-            if (client == null || PasswordHasher.VerifyPassword(client.Pass, PasswordHasher.HashPassword(textBox2.Text)))
-            Console.WriteLine("User que fez login : " + client.Email);
-            Console.WriteLine("Pass que fez login : " + client.Pass);
-            Console.WriteLine("Pass introduzida : " + textBox2.Text);
+
+            if (client == null)
+            {
+                MessageBox.Show("Não existe Registo para este Username");
+                return;
+            }
+
             if (client == null || !PasswordHasher.VerifyPassword(textBox2.Text, client.Pass))
             {
                 PassRecovery passRecoveryForm = new PassRecovery("Credenciais não válidas! Deseja Receber um Email para fazer Reset á sua Password?", textBox1.Text);
@@ -38,9 +41,6 @@ namespace SpinToWin
             }
             else
             {
-                Console.WriteLine("Username" + client.Email);
-                Console.WriteLine("Pass na BD" + client.Pass);
-                Console.WriteLine("Pass no login" + PasswordHasher.HashPassword(textBox2.Text));
                 Global.accountID = client.Email.Equals("admin") ? 0 : (int)client.Id;
                 showHomeForm(true);
             }
@@ -86,7 +86,6 @@ namespace SpinToWin
             }
             string pass = PasswordHasher.HashPassword(textBox2.Text);
             clientDAO.InsertClient(new Client(textBox1.Text, pass, 0));
-            Console.WriteLine("Pass no registo: " + pass);
             MessageBox.Show("Registo efetuado com sucesso.");
         }
 
